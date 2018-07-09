a170024b94a065eb3a564ddef31a9411c09234ad
Roni <roni@Ronis-Mac-mini.local>
2018-07-09 17:26:03 +0300

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,49 +10,6 @@ import { IApartments } from '../iapartments';
 })
 export class MapComponent implements OnInit {
   apartments = [];
-  /*apartmentModal = {
-    id: "",
-    favorite: false,
-    name: "",
-    email: "",
-    apartment: {
-      id: "",
-      location: "",
-      info: "",
-      latlng: {
-        lat: 1,
-        lng: 1
-      },
-      images: [
-        ""
-      ],
-      phones: [""],
-      apartmentType: "",
-      rooms: 1,
-      parking: 1,
-      balcony: 1,
-      mamad: false,
-      elevator: false,
-      air_conditioner: false,
-      tama: false,
-      reconditioned: false,
-      bars: false,
-      master_room: false,
-      storage: false,
-      gym: false,
-      furniture: false,
-      pets: false,
-      roomates: false,
-      immediate_entrance: false,
-      apartment_image: false,
-      price: 1,
-      size: 1,
-      floor: 1,
-      entrence_date: "",
-      toilets: 1
-    }
-  };
-  */
  apartmentModal = {};
   lat: number = 32.056442;
   lng: number = 34.772238;
